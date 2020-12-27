97cf87c396019b4f313c3a72ec3212ad8486dcb9
Yishuai Li <yishuai@upenn.edu>
2020-12-27 01:23:24 -0500

    Skip 404 Not Found

--- 2.4/core.c
+++ 2.4/core.c
@@ -4757,7 +4757,6 @@ static int default_handler(request_rec *r)
             ap_log_rerror(APLOG_MARK, APLOG_INFO, 0, r, APLOGNO(00128)
                           "File does not exist: %s",
                           apr_pstrcat(r->pool, r->filename, r->path_info, NULL));
-            return HTTP_NOT_FOUND;
         }
 
         /* Don't try to serve a dir.  Some OSs do weird things with
